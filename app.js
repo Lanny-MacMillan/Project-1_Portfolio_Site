8adb934e1153f6ddf51817c8f9f5b1ca6b0a9cba
Lanny Mac <Lanny@MacBook-Pro.local>
2022-04-27 17:12:41 -0400

    layout finalized all links, icons and pages working, added buttons to navigate pages, buttons not functional yet

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('THIS IS A TEST')
+const spaceBattle = () => {
+    window.open('https://fanciful-caramel-fdd279.netlify.app/', "_blank");
+}
+const RockPaperScissors = () => {
+    window.open(url, "_blank");
+}
+const home = () => {
+    open('index.html');
+}
+const about = () => {
+    // open('about.html');
+    url('about.html')
+}
+const resume = () => {
+    open('resume.html');
+}
+const projects = () => {
+    open('projects.html');
+}
+console.log()
+$(()=>{
+    $('#sb').on('click', spaceBattle);
+    $('#rps').on('click', RockPaperScissors);
+    $('#1').on('click', home);
+    $('#2').on('click', about);
+    $('#3').on('click', resume);
+    $('#4').on('click', projects);
+
+
+
+
+});
+
+<!-- <a href="index.html">Home</a>
+<a href="about.html">About</a>
+<a href="resume.html">Resume</a>
+<a href="projects.html">Projects</a> -->
