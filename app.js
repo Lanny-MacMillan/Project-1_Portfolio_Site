9a0fc6d7982d98655dffac88338e22b6d602b32d
Lanny Mac <Lanny@MacBook-Pro.local>
2022-04-28 01:28:17 -0400

    buttons and links functional, basic framework finalized

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+$(()=>{
+
 const spaceBattle = () => {
     window.open('https://fanciful-caramel-fdd279.netlify.app/', "_blank");
 }
 const RockPaperScissors = () => {
-    window.open(url, "_blank");
+    window.open('https://comforting-pithivier-c81117.netlify.app/', "_blank");
 }
 const home = () => {
-    open('index.html');
+    window.location.href = 'index.html';
 }
 const about = () => {
-    // open('about.html');
-    url('about.html')
+    window.location.href = 'about.html';
 }
 const resume = () => {
-    open('resume.html');
+    window.location.href = 'resume.html';
 }
 const projects = () => {
-    open('projects.html');
+    window.location.href = 'projects.html';
 }
-console.log()
-$(()=>{
+
+
+// ============================================    Event Listeners   =============================================
+
     $('#sb').on('click', spaceBattle);
     $('#rps').on('click', RockPaperScissors);
     $('#1').on('click', home);
@@ -27,11 +30,44 @@ $(()=>{
     $('#4').on('click', projects);
 
 
-
-
 });
 
-<!-- <a href="index.html">Home</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//============================================   THE GRAVEYARD ==================================================
+{/* <a href="index.html">Home</a>
 <a href="about.html">About</a>
 <a href="resume.html">Resume</a>
-<a href="projects.html">Projects</a> -->
+<a href="projects.html">Projects</a> */}
+
+//wont clear page on multiple clicks
+// const about = () => {
+//     $('#middle').remove()
+//     $('<h5>').remove()
+//     $('</br>').remove()
+//     const $br = $('</br>')
+
+//     const $h5a = $('<h5>').text('Ive been working in the Electrical industry for 25 years. It started with an apprenticeship at a small company when I was 15 and that brought me to getting my electrical license 5 years later. With that license I became a project foreman/lead electrician running projects valued from $50k to $3 million with a crew of up to 15 people.')
+//     const $h5b = $('<h5>').text('After a few years of successful project completions I was promoted to Project Manager overseeing the same projects I had been running. This was a good fit but I excelled at problem solving and troubleshooting electrical issues. This led me to start my own electrical service company, I really enjoyed the business side of it. I had my own company for a few years and we did so well that the company I left bought me out and had me run a service division within that company. We grew and expanded the division, hired more electricians and bought more service vehicles.')
+//     const $h5c = $('<h5>').text("But over the years I lost alot of the passion and fun I had doing Electrical, so I started to revisit programming which I enjoyed alot in High School. Im currently at General Assembly's Software Engineering Immersive Program learning to be a software developer, looking forward to a new future.")
+//     $('#lower').append($br)
+//     $('#lower').append($h5a)
+//     $('#lower').append($br)
+//     $('#lower').append($h5b)
+//     $('#lower').append($br)
+//     $('#lower').append($h5c)
+//     $('#lower').append($br)
+
+// }
