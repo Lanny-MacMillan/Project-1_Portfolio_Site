5b4e397eea4719b90c06d228bc16ccc71ca80007
Lanny Mac <Lanny@MacBook-Pro.local>
2022-05-02 22:56:50 -0400

    readme complete

--- README.md
+++ README.md
@@ -1,26 +1,19 @@
 # Project-1_Portfolio_Site
-Project 1 - portfolio site/app
 
-You will be building a portfolio site for yourself.
+# Site link https://lanny-macmillan-portfolio.netlify.app/
 
-# 🔴 Mandatory To Pass:
-# MVP - Minimum Viable Product
-# Your application must meet these requirements:
-# Built with HTML, CSS, JavaScript and jQuery.
-# Hosted on Netlify or Github Pages.
-# Commits to Github every day.
-# A README.md file with explanations of the technologies used, the approach taken, a link to your live site, installation instructions, unsolved problems, etc.
-# The app must:
-# Have the following pages:
-# home (greeting page)
-# bio (your background)
-# resume (past work experience)
-# projects (a place to hold interesting projects that you build)
-# links (links to your github, linkedin, and other sites you find interesting)
-# optionally, instead of making this a separate page, you can make these links appear as part of a navigation section that appears on all pages
-# Use JS/JQuery to create at least three points of interaction for the user (e.g. the user clicks on something and something happens). Write these from scratch. Please don't use 3rd party frameworks/libraries like jQuery UI, bootstrap, etc.
-# Implement responsive design (i.e. it should be fully functional on desktop, tablet, mobile, etc.).
-# 🔵 Stretch Goals (Not Mandatory):
-# Create a complex user interface module (e.g. a carousel, modal, sticky nav, etc)
-# Create an additional browser-based game (e.g. a quiz, choose your own adventure, battle game, etc)
-# Research AJAX (covered later in this course) and create an app that displays interesting data from an external source (ask your instructor for a list of good sources)
+# Approach taken and Technologies
+
+# In my portfolio site I tried to make something like an interactive buisiness card, I have multiple Div containers set inside each other for a simple clean look with some shadowboxing. Each section of the webpage can be navigated through the button icons at the top. I added a bouncing animation to them so the page has a little life and isnt so static. A few of the buttons have a text change on hover, Projects one was done in jQuery with mouseover function and the other 2 (embedded pages in the projects menu) are done with a hover function in a <span>. Projects page has 2 interactive games to play, both have a wiggle effect and changes text on hover. Both interactive games will open in new tabs.
+# Social Media links are done with icons donwloaded and then I styled them with a hover effect that greys out the icon as well as flashes through the colors that the company icon represents. FB is blue, Reddit is red, LinkedIn is blue and white, Instagram uses purple, red, yellow and magenta and google uses red, yellow, blue and green. All colors were meant to match company color scheme.
+# Contact button opens a modal window with a MAILTO: form, its not what I would like to do but I wasnt sure if using prewritten forms for php contact was allowed.
+# Resume and Projects pages are embedded within the center DIVs. I thought this may be best for easily updating my Resume or Projects page. Both arent fixed to this website and can be used for other purposes, like updating or emailing my resume or project pages by them selves.
+
+
+# Unsolved or Future to do:
+
+# 1) Id like to have the interactive games open in their own modal windows and not take you off the page.
+# 2) Add a photo carosel to the About Me section
+# 3) Set up a php contact form
+# 4) Have scroll bars transparent, they dont show up on mobile but do in my browser window.
+# 5) Chnage center DIV box to inline block instead of FLEX, it changes size slightly to make room for content, not visible on PC but can be seen on mobile
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(()=>{
-//======================================= MODAL ==================================================
+//======================================= MODAL ======================================
 const $modal = $('#modal')
 const $closeBtn = $('#close')    
 const openModal = () => {
@@ -9,6 +9,7 @@ const closeModal = () => {
     $modal.css('display', 'none')
 }
 
+//================================== Backgrounds ========================================
 let backgroundArray = []
 //Storing these urls didnt seem to work, but pushing them into the array did
 backgroundArray.push('https://i.imgur.com/VOgGD0x.jpg')
@@ -27,6 +28,8 @@ randomBackground = () => {
 const home = () => {
 window.location.href = 'index.html';
 
+//================================= On Click Functions ==================================
+
 
 }
 const about = () => {
@@ -38,9 +41,11 @@ const resume = () => {
 const projects = () => {
     window.location.href = 'projects.html';
 }
-const contact = () => {
-    window.location.href = 'contact.html';
-}
+// const contact = () => {
+//     window.location.href = 'contact.html';
+// }
+
+//================================= Mouseover Functions ==================================
 
 
 // mouseover projects link and change text to interactive back to projects
@@ -52,7 +57,7 @@ btn.addEventListener("mouseout", function() {
     this.textContent = "Projects";
 })
 
-// ============================================    Event Listeners   =============================================
+// ==================================== Event Listeners ===================================
 
     $('#1').on('click', home);
     // $('#1').on('click', randomBackground());
@@ -67,18 +72,7 @@ btn.addEventListener("mouseout", function() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-//============================================   THE GRAVEYARD ==================================================
+//====================================  THE GRAVEYARD ===================================
 // const randomBackground = () => {
 //     let num = Math.floor(Math.random() * background.length)
 //     if (num === 1) {
@@ -93,37 +87,16 @@ btn.addEventListener("mouseout", function() {
 
 
 
-
-
-
-
 {/* <a href="index.html">Home</a>
 <a href="about.html">About</a>
 <a href="resume.html">Resume</a>
 <a href="projects.html">Projects</a> */}
 
 //wont clear page on multiple clicks
-// const about = () => {
-//     $('#middle').remove()
-//     $('<h5>').remove()
-//     $('</br>').remove()
-//     const $br = $('</br>')
-
-//     const $h5a = $('<h5>').text('Ive been working in the Electrical industry for 25 years. It started with an apprenticeship at a small company when I was 15 and that brought me to getting my electrical license 5 years later. With that license I became a project foreman/lead electrician running projects valued from $50k to $3 million with a crew of up to 15 people.')
-//     const $h5b = $('<h5>').text('After a few years of successful project completions I was promoted to Project Manager overseeing the same projects I had been running. This was a good fit but I excelled at problem solving and troubleshooting electrical issues. This led me to start my own electrical service company, I really enjoyed the business side of it. I had my own company for a few years and we did so well that the company I left bought me out and had me run a service division within that company. We grew and expanded the division, hired more electricians and bought more service vehicles.')
-//     const $h5c = $('<h5>').text("But over the years I lost alot of the passion and fun I had doing Electrical, so I started to revisit programming which I enjoyed alot in High School. Im currently at General Assembly's Software Engineering Immersive Program learning to be a software developer, looking forward to a new future.")
-//     $('#lower').append($br)
-//     $('#lower').append($h5a)
-//     $('#lower').append($br)
-//     $('#lower').append($h5b)
-//     $('#lower').append($br)
-//     $('#lower').append($h5c)
-//     $('#lower').append($br)
-
-// }
 
 
-//========================= Change background image based on cookie visits ===================================
+
+//================= Change background image based on cookie visits =======================
 
 
 // var name = "visitCount" + "=";
